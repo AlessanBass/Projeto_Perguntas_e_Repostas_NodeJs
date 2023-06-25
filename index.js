8159c0d30f653375d394fc92f22ebef371e81591
Alessandro Conceição Santos <alessanbaixista@gmail.com>
2023-06-25 01:33:59 -0300

    Configurando e exibindo HTML com EJS

--- index.js
+++ index.js
@@ -1,14 +1,28 @@
 const express = require("express"); //Importando o Express
 const app = express(); // Iniciando o express
+const porta = 4000;
 
+//Utilizando para processos de configuração
+/* 
+    Por padrão é necessário criar uma pasta chmada "view" onde
+    vamos colocar todo o nosso html para que o ejs possa acessar.
+*/
+app.set('view engine', 'ejs'); //Seta o ejs
 
-
-
+//Rota principal da aplicação
+app.get("/", function(req, res){
+    //Mostrando o html
+    /* 
+        Neste caso não precisamos passar o caminho completo,
+        pois o "render" olha direto na pasta view
+    */
+    res.render("index")
+});
 //Criando o servidor
-app.listen(4000, function(erro){
+app.listen(porta, function(erro){
     if(erro){
         console.log(erro)
     }else{
-        console.log("Servidor iniciado com sucesso!");
+        console.log("Servidor rodando na porta " + porta + "!");
     }
 })
